cdcf4d0eaa5d40de32da072e6f16d33b49c9ea14
Timothy Chung <timchunght@gmail.com>
2017-04-11 21:38:56 -0400

    Updated left scraper

--- left_scraper.py
+++ left_scraper.py
@@ -4,17 +4,21 @@ from io import StringIO, BytesIO
 
 
 
+def left_url_content(url):
+	document = requests.get(url)
+	html_text = document.json()["body"]
+	# tree = html.fromstring(page.content)
 
-document = requests.get("http://history-lab.org/api/documents/frus1977-80v26d241")
-html_text = document.json()["body"]
-# tree = html.fromstring(page.content)
+	# print tree
 
-# print tree
+	parser = etree.HTMLParser()
+	tree = etree.parse(StringIO(html_text), parser)
 
-parser = etree.HTMLParser()
-tree = etree.parse(StringIO(html_text), parser)
+	result = etree.tostring(tree.getroot(),
+	pretty_print=True, method="html")
 
-result = etree.tostring(tree.getroot(),
-pretty_print=True, method="html")
+	print result
+	return html_text
 
-print result
+if __name__ == "main":
+	left_url_content("")
