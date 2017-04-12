d2d5d8e4c418768defcabac065af321fa5b07131
Timothy Chung <timchunght@gmail.com>
2017-04-11 22:02:59 -0400

    Refactored compare

--- compare.py
+++ compare.py
@@ -2,19 +2,34 @@ from fuzzywuzzy import fuzz
 from right_scraper import right_url_content
 from left_scraper import left_url_content
 
-left_url = "history-lab.org/documents/frus1969-76ve08d194"
-right_url = "history-lab.org/documents/1975NEWDE04555"
-raw_left = left_url_content(left_url)
-new_left = "1." + "".join(raw_left.split("1.")[1:])
-# print(new_left)
 
-raw_right = right_url_content(right_url)
-new_right = "1." + "".join(raw_right.split("1.")[1:])
+def compare(left_url, right_url):
+	raw_left = left_url_content(left_url)
+	new_left = "1." + "".join(raw_left.split("1.")[1:])
+	# print(new_left)
 
-cleaned_left = new_left.lower().replace('\n', '').replace('\r', '').replace(" ", "")
-cleaned_right = new_right.lower().replace('\n', '').replace('\r', '').replace(" ", "")
-print(cleaned_left)
-print("===================================")
-print(cleaned_right)
+	raw_right = right_url_content(right_url)
+	new_right = "1." + "".join(raw_right.split("1.")[1:])
 
-print(fuzz.partial_ratio(cleaned_left, cleaned_right))
+	cleaned_left = new_left.lower().replace('\n', '').replace('\r', '').replace(" ", "")
+	cleaned_right = new_right.lower().replace('\n', '').replace('\r', '').replace(" ", "")
+	print(cleaned_left)
+	print("===================================")
+	print(cleaned_right)
+
+	percent_match = fuzz.partial_ratio(cleaned_left, cleaned_right)
+	return percent_match
+
+
+
+if __name__ == "__main__":
+	left_url = "history-lab.org/documents/frus1969-76ve08d194"
+	right_url = "history-lab.org/documents/1975NEWDE04555"
+	percent_match = compare(left_url, right_url)
+	print(percent_match)
+	if percent_match > 80:
+		print "Match"
+	elif percent_match > 50:
+		print "Partial"
+	else:
+		print "error"
